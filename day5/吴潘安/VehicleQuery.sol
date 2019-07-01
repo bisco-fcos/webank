df22bb2649da1b6d161ae887794fba8a7cc78d5d
Z edom <w468750@126.com>
2019-07-02 00:40:18 +0800

    upload modified contracts

---
+++ day5/吴潘安/VehicleQuery.sol
@@ -0,0 +1,106 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+import "./VehicleUpdate.sol";
+import "./ERC721.sol";
+
+contract VehicleQuery is VehicleUpdate, ERC721 {
+    
+     //获取汽车维护信息
+    function getVehicleManufacturingInfo(string memory VIN) public view returns (string memory) {
+        bool exist = VINExist[VIN];
+         require( exist == true,"This VIN is currently invalid,please check the number carefully");
+         uint carindex = VINtoVehicle[VIN];
+        Vehicle memory targetcar = cars[carindex];
+        return targetcar.ManufacturingInfo;
+    }
+    
+    //index为维修记录的下标,获取维修保养注信息
+    function getInfo(string memory VIN, uint index) public view returns (string memory) {
+        bool exist = VINExist[VIN];
+         require( exist == true,"This VIN is currently invalid,please check the number carefully");
+         uint carindex = VINtoVehicle[VIN];
+        Vehicle memory targetcar = cars[carindex];
+        return targetcar.records[index]._info;
+    }
+     //index为维修记录的下标,获取维修点信息
+    function getAddress(string memory VIN, uint index) public view returns (address) {
+        bool exist = VINExist[VIN];
+         require( exist == true,"This VIN is currently invalid,please check the number carefully");
+         uint carindex = VINtoVehicle[VIN];
+        Vehicle memory targetcar = cars[carindex];
+        return targetcar.records[index]._MaintenanceShop;
+    }
+     //获取汽车维修记录条数
+    function getNumsOfRecords(string memory VIN) public view returns (uint) {
+        bool exist = VINExist[VIN];
+         require( exist == true,"This VIN is currently invalid,please check the number carefully");
+         uint carindex = VINtoVehicle[VIN];
+        Vehicle memory targetcar = cars[carindex];
+        return targetcar.records.length;
+    }
+    
+    //index为维修记录的下标,获取维修备注信息
+     function getRemark(string memory VIN, uint index) public view returns (string memory) {
+        bool exist = VINExist[VIN];
+         require( exist == true,"This VIN is currently invalid,please check the number carefully");
+         uint carindex = VINtoVehicle[VIN];
+        Vehicle memory targetcar = cars[carindex];
+        return targetcar.records[index]._remarks;
+    }
+    
+    //index为维修记录的下标,获取维修时间戳信息
+    function getTimeStamp(string memory VIN, uint index) public view returns (uint) {
+        bool exist = VINExist[VIN];
+         require( exist == true,"This VIN is currently invalid,please check the number carefully");
+         uint carindex = VINtoVehicle[VIN];
+        Vehicle memory targetcar = cars[carindex];
+        return targetcar.records[index]._time;
+    }
+    
+    function getTotalInfo(string memory VIN, uint index) public view returns (string memory,string memory,address,uint) {
+        bool exist = VINExist[VIN];
+         require( exist == true,"This VIN is currently invalid,please check the number carefully");
+         uint carindex = VINtoVehicle[VIN];
+        Vehicle memory targetcar = cars[carindex];
+        return (targetcar.records[index]._info,targetcar.records[index]._remarks,targetcar.records[index]._MaintenanceShop,targetcar.records[index]._time);
+    }
+    
+    
+    mapping (uint => address) carApprovals;
+    
+    function balanceOf(address _owner) public view returns (uint256 _balance) {
+        return ownerCarCount[_owner];
+    }
+    
+    function ownerOf(uint256 _tokenId) public view returns (address _owner) {
+        require( _tokenId < cars.length ,"This tokenID is currently invalid,please check the number carefully");
+        return carToOwner[_tokenId];
+    }
+    
+    function _transfer(address _from, address _to, uint256 _tokenId) private {
+        require( _tokenId < cars.length ,"This tokenID is currently invalid,please check the number carefully");
+        ownerCarCount[_to]++;
+        ownerCarCount[_from]--;
+        carToOwner[_tokenId] = _to;
+        emit Transfer(_from, _to, _tokenId);
+    }
+    
+    function transfer(address _to, uint256 _tokenId) public onlyOwnerOfCar(_tokenId) {
+        require( _tokenId < cars.length ,"This tokenID is currently invalid,please check the number carefully");
+        _transfer(msg.sender, _to, _tokenId);
+    }
+    
+    function approve(address _to, uint256 _tokenId) public onlyOwnerOfCar(_tokenId) {
+        require( _tokenId < cars.length ,"This tokenID is currently invalid,please check the number carefully");
+        carApprovals[_tokenId] = _to;
+        emit Approval(msg.sender, _to, _tokenId);
+    }
+    
+    function takeOwnership(uint256 _tokenId) public {
+        require( _tokenId < cars.length ,"This tokenID is currently invalid,please check the number carefully");
+        require(carApprovals[_tokenId] == msg.sender);
+        address owner = ownerOf(_tokenId);
+        _transfer(owner, msg.sender, _tokenId);
+    }
+}
+
---
+++ day5/吴潘安/VehicleUpdate.sol
@@ -0,0 +1,100 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+
+
+contract VehicleUpdate {
+    
+    address[] internal ApprovedMaintenanceShop = new address[](0);//授权维修点地址集合
+    address internal Administrator;//合约部署者，管理员
+    
+    // 车辆动态数组
+    Vehicle [] internal cars;
+    //以车架号对应车辆索引
+    mapping (string => uint) internal  VINtoVehicle;
+    // 车辆索引对应主人地址
+    mapping (uint => address) public carToOwner;
+    // 车主拥有车辆计数
+    mapping (address => uint) ownerCarCount;
+    mapping (string => bool) VINExist;
+    
+    constructor () public{
+        ApprovedMaintenanceShop.push(msg.sender);
+        Administrator = msg.sender;
+    }
+    
+    // 保养记录类
+    struct MaintenanceRecord {
+        string _remarks;//备注
+        string _info;//维修保养的具体信息
+        address _MaintenanceShop;//记录维修地点
+        uint _time;
+    }
+    
+    
+    // 车辆类
+    struct  Vehicle {
+        // 车架号
+        string VIN;
+        string ManufacturingInfo;//汽车出厂信息
+        MaintenanceRecord[] records;//车辆所拥有的维修保养信息
+    }
+    
+    
+    //只有允许的维修点才能执行的修饰函数
+    modifier onlyMaintenanceShop(address){
+        bool label=false;
+        for(uint cnt;cnt<ApprovedMaintenanceShop.length;cnt++)
+        {
+            if(msg.sender == ApprovedMaintenanceShop[cnt])
+                label = true;
+        }
+        require(label == true,"This function can only be exerted by the MaintenanceShop");
+        _;
+    }
+    
+    // 只有车主才有车的交易权
+    modifier onlyOwnerOfCar(uint carID) {
+        require(msg.sender == carToOwner[carID]);
+        _;
+    }
+
+    //只有管理员才能执行的修饰函数
+     modifier onlyAdministrator(address){
+         require(msg.sender == Administrator,"This function can only be exerted by the Administrator");
+        _;
+    }
+    
+    function addApprovedMaintenanceShop(address approvedAddress) public onlyAdministrator(msg.sender){
+        ApprovedMaintenanceShop.push(approvedAddress);
+    }
+    
+     //出厂信息设置
+    function ManufactureInit(string memory VIN, string memory originInfo) public onlyMaintenanceShop(msg.sender) {
+        bool exist = VINExist[VIN];
+        require( exist == false,"The ManufacturingInfo is already initialized");
+        
+        uint index = VINtoVehicle[VIN];
+        Vehicle storage car = cars[index];
+        VINExist[VIN] = true;
+        
+        car.ManufacturingInfo =(originInfo);
+        ownerCarCount[msg.sender] += 1;
+        cars.push(car);
+        carToOwner[index] = msg.sender;
+        
+    }
+    
+    //更新汽车维护信息
+    function updateVehicleMaintenance (string memory VIN,string memory remarks,string memory info) public onlyMaintenanceShop(msg.sender){
+         bool exist = VINExist[VIN];
+         require( exist == true,"This VIN is currently invalid,please check the number carefully");
+         uint index = VINtoVehicle[VIN];
+         Vehicle storage targetcar = cars[index];
+         MaintenanceRecord memory newRecord;
+         newRecord._remarks=remarks;
+         newRecord._info=info;
+         newRecord._MaintenanceShop = msg.sender;
+         newRecord._time =now; 
+         targetcar.records.push(newRecord);
+    }
+}
